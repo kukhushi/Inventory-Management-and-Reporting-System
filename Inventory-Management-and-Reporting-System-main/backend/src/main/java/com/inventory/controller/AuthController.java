05c37461ee5df8a2a7705a811efa9c85454a14d0
kukhushi <kukhushi0809@gmail.com>
2026-04-17 20:32:09 +0530

    Initial project commit

---
+++ Inventory-Management-and-Reporting-System-main/backend/src/main/java/com/inventory/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.inventory.controller;
+
+import com.inventory.dto.AuthRequest;
+import com.inventory.dto.AuthResponse;
+import com.inventory.dto.RegisterRequest;
+import com.inventory.authservice.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            return ResponseEntity.ok(authService.register(request));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
+        try {
+            return ResponseEntity.ok(authService.login(request));
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body(e.getMessage());
+        }
+    }
+}
